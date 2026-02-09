8860f17f7bb0b4f1e2f23f87981faafb556ab46f
lolesch <leonid.schreiber@yahoo.de>
2026-02-09 22:47:08 +0100

    updated previewIdonDrawer

--- Attributes/Editor/PreviewIconDrawer.cs
+++ Attributes/Editor/PreviewIconDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -9,43 +10,100 @@ namespace Submodules.Utility.Attributes.Editor
     {
         private const float _textureSize = 64;
 
-        public override float GetPropertyHeight( SerializedProperty property, GUIContent label )
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 
+            property.objectReferenceValue is Sprite ? _textureSize : EditorGUIUtility.singleLineHeight;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            return property.objectReferenceValue != null ? _textureSize : base.GetPropertyHeight( property, label );
-        }
-
-        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
-        {
-            position = EditorGUI.IndentedRect( position );
-
-            _ = EditorGUI.BeginProperty( position, label, property );
-
+            position = EditorGUI.IndentedRect(position);
+            EditorGUI.BeginProperty(position, label, property);
+            
+            int prevIndent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
 
-            var labelRect = new Rect( position.x, position.y, EditorGUIUtility.labelWidth,
-                EditorGUIUtility.singleLineHeight );
+            int controlID = property.propertyPath.GetHashCode();
+            Sprite sprite = property.objectReferenceValue as Sprite;
 
-            GUI.Label( labelRect, property.displayName );
-
-            var spriteRect = new Rect( labelRect.x + EditorGUIUtility.labelWidth, position.y, _textureSize,
-                _textureSize );
-
-            if ( property.objectReferenceValue != null )
+            if (sprite == null)
             {
-                property.objectReferenceValue =
-                    EditorGUI.ObjectField( spriteRect, property.objectReferenceValue, typeof(Sprite), false );
-
-                //if this is not a repaint or the property is null exit now
-                if ( Event.current.type != EventType.Repaint || property.objectReferenceValue == null )
-                    return;
+                Rect fieldRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+                EditorGUI.PropertyField(fieldRect, property, label);
             }
             else
             {
-                _ = EditorGUI.PropertyField( spriteRect, property, true );
+                Rect labelRect = new(position.x, position.y, EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight);
+                GUI.Label(labelRect, label);
+
+                // Draw preview instead of picker
+                Rect previewRect = new(labelRect.x + EditorGUIUtility.labelWidth, position.y, _textureSize, _textureSize);
+
+                if (Event.current.type == EventType.Repaint)
+                {
+                    var tex = sprite.texture;
+                    Rect texCoords = sprite.rect;
+                    texCoords.x /= tex.width;
+                    texCoords.y /= tex.height;
+                    texCoords.width /= tex.width;
+                    texCoords.height /= tex.height;
+
+                    GUI.DrawTextureWithTexCoords(previewRect, tex, texCoords);
+                    GUI.Box(previewRect, GUIContent.none, EditorStyles.helpBox);
+                }
+
+                // Open picker on click
+                if ( Event.current.type == EventType.MouseDown &&
+                     Event.current.button == 0 &&
+                     previewRect.Contains(Event.current.mousePosition))
+                {
+                    EditorGUIUtility.ShowObjectPicker<Sprite>(sprite, false, null, controlID);
+                    Event.current.Use();
+                }
             }
 
+            // Handle picker update or cleared selection
+            if ((Event.current.commandName == "ObjectSelectorUpdated" ||
+                 Event.current.commandName == "ObjectSelectorClosed") &&
+                EditorGUIUtility.GetObjectPickerControlID() == controlID)
+            {
+                property.objectReferenceValue = EditorGUIUtility.GetObjectPickerObject() as Sprite;
+                property.serializedObject.ApplyModifiedProperties();
+
+                GUI.changed = true;
+                EditorUtility.SetDirty(property.serializedObject.targetObject);
+                InternalEditorUtility.RepaintAllViews();
+            }
+
+            EditorGUI.indentLevel = prevIndent;
             EditorGUI.EndProperty();
         }
+
+
+        //public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        //{
+        //    position = EditorGUI.IndentedRect(position);
+        //    EditorGUI.BeginProperty(position, label, property);
+        //    EditorGUI.indentLevel = 0;
+//
+        //    var labelRect = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight);
+        //    GUI.Label(labelRect, property.displayName);
+        //    var spriteRect = new Rect(labelRect.x + EditorGUIUtility.labelWidth, position.y, _textureSize, _textureSize);
+//
+        //    property.objectReferenceValue = EditorGUI.ObjectField( spriteRect, property.objectReferenceValue, typeof(Sprite), false );
+//
+        //    if( property.objectReferenceValue is Sprite sprite && sprite.texture != null && Event.current.type == EventType.Repaint )
+        //    {
+        //        var texture = sprite.texture;
+        //        var texCoords = sprite.rect;
+        //        texCoords.x /= texture.width;
+        //        texCoords.y /= texture.height;
+        //        texCoords.width /= texture.width;
+        //        texCoords.height /= texture.height;
+//
+        //        GUI.DrawTextureWithTexCoords( spriteRect, texture, texCoords );
+        //    }
+//
+        //    EditorGUI.EndProperty();
+        //}
     }
 }
 #endif
