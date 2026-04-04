e0e492ee2fecbb28c599802b8f72f478ce441dff
lolesch <leonid.schreiber@yahoo.de>
2026-04-04 23:58:30 +0200

    using bitshift instead of division and removed unnsed methods for now

--- Extensions/HexExtensions.cs
+++ Extensions/HexExtensions.cs
@@ -5,20 +5,9 @@ namespace Submodules.Utility.Extensions
     public static class HexExtensions
     {
         /// <summary>
-        /// Converts a cell position to Hex position.
+        /// Converts Unity's Grid cell position to Hex position.
         /// </summary>
-        //NOTE: inverted cell.y to match unity's grid with y pointing up
-        public static Hex CellToHex(this Vector3Int cell) => new(cell.x - (-cell.y - (-cell.y & 1)) / 2, -cell.y);
-        public static Hex PixelToHex(this Vector3 pixel, Grid hexGrid) => CellToHex(hexGrid.WorldToCell(pixel));
-        public static Hex PixelToHex(this Vector2 pixel, float hexSize, Vector2Int spacing, Vector2 origin)
-        {
-            pixel -= origin;
-            var q = (Mathf.Sqrt(3) / 3 * pixel.x - 1f / 3 * pixel.y) / (hexSize + spacing.x);
-            var r = 2f / 3 * pixel.y / (hexSize + spacing.y);
-
-            return new FloatHex(q, r).Round();// * new Vector2Int(1, -1);
-        }
-        public static Hex GUIToHex(this Vector2 pixel, float hexSize, Vector2Int spacing, Vector2 origin) => PixelToHex(pixel * new Vector2(1, -1), hexSize, spacing, origin * new Vector2(1, -1));
+        public static Hex CellToHex(this Vector3Int cell) => new(cell.x + ((cell.y + (cell.y & 1)) >> 1), -cell.y);
         
         public static Hex WorldToHex(this Vector3 worldPosition, float hexWidth, float hexCircumradius )
         {
@@ -30,5 +19,26 @@ namespace Submodules.Utility.Extensions
            
             return new Hex( q, r );
         }
+        //public static Hex WorldToHex(this Vector3 worldPosition, float size) => new Vector2(worldPosition.x, worldPosition.z).PixelToHex(size);
+        
+        //public static Hex PixelToHex(this Vector3 pixel, Grid hexGrid) => CellToHex(hexGrid.WorldToCell(pixel));
+        /*public static Hex PixelToHex(this Vector2 pixel, float hexSize, Vector2Int spacing, Vector2 origin)
+        {
+            pixel -= origin;
+            var q = (Mathf.Sqrt(3) / 3 * pixel.x - 1f / 3 * pixel.y) / (hexSize + spacing.x);
+            var r = 2f / 3 * pixel.y / (hexSize + spacing.y);
+
+            return new FloatHex(q, r).Round();// * new Vector2Int(1, -1);
+        }*/
+        
+        /*public static Hex PixelToHex(this Vector2 pixel, float size) 
+        {
+            var q = (Mathf.Sqrt(3) / 3 * pixel.x - 1f / 3 * pixel.y) / size;
+            var r = 2f / 3 * pixel.y / size;
+            return new FloatHex(q, r).Round();
+        }*/
+
+        //public static Hex GUIToHex(this Vector2 guiPos, float size) => new Vector2(guiPos.x, -guiPos.y).PixelToHex(size);
+        //public static Hex GUIToHex(this Vector2 guiPos, float hexSize, Vector2Int spacing, Vector2 origin) => PixelToHex(new Vector2(guiPos.x, -guiPos.y), hexSize, spacing, new Vector2(origin.x, -origin.y));
     }
 }
